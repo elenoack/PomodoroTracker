5e16d0e10e4768ac502569538a90828c2b7eced5
elenoack <elenanoack@mail.ru>
2022-05-20 12:41:09 +0300

    add-aytoLayout + configuration-CALayer + add-Image

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -123,7 +123,121 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createCircularPath()
+        setupView()
+        setupLayout()
     }
+}
+
+// MARK: - Private
+
+private extension ViewController {
+    
+    func setupView() {
+        view.addSubviewsForAutoLayout([
+            background,
+            tomatoWorkView,
+            tomatoStopView,
+            workLabel,
+            stopLabel,
+            workButton,
+            stopButton,
+        ])
+        
+        view.layer.addSublayer(circleLayer)
+        view.layer.addSublayer(progressLayer)
+        
+        view.layer.addSublayer(circleStopLayer)
+        view.layer.addSublayer(progressStopLayer)
+        
+        view.addSubviewsForAutoLayout([
+            plantView,
+            plantStopView,
+        ])
+    }
+    
+    func setupLayout() {
+        NSLayoutConstraint.activate([
+            background.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            background.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            background.widthAnchor.constraint(equalTo: view.widthAnchor),
+            background.heightAnchor.constraint(equalTo: view.heightAnchor),
+            
+            workLabel.bottomAnchor.constraint(equalTo: workButton.topAnchor, constant: -Static.indent),
+            workLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            stopLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: Static.centerStopIndent),
+            stopLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            workButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            workButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -Static.centerIndent),
+            workButton.heightAnchor.constraint(equalToConstant: Static.heightButton),
+            workButton.widthAnchor.constraint(equalToConstant: Static.widhtButton),
+            
+            stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stopButton.topAnchor.constraint(equalTo: stopLabel.bottomAnchor, constant: Static.indent),
+            stopButton.heightAnchor.constraint(equalToConstant: Static.heightButton),
+            stopButton.widthAnchor.constraint(equalToConstant: Static.widhtButton),
+            
+            tomatoWorkView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            tomatoWorkView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -Static.centerIndent - (Static.indent * 2)),
+            tomatoWorkView.heightAnchor.constraint(equalToConstant: Static.circleSize),
+            tomatoWorkView.widthAnchor.constraint(equalToConstant: Static.circleSize),
+            
+            plantView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            plantView.heightAnchor.constraint(equalToConstant: Static.heightPlant),
+            plantView.widthAnchor.constraint(equalToConstant: Static.widhtPlant),
+            plantView.bottomAnchor.constraint(equalTo: tomatoWorkView.topAnchor),
+            
+            tomatoStopView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            tomatoStopView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: Static.centerIndent + (Static.indent * 2)),
+            tomatoStopView.heightAnchor.constraint(equalToConstant: Static.circleSize),
+            tomatoStopView.widthAnchor.constraint(equalToConstant: Static.circleSize),
+            
+            plantStopView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            plantStopView.heightAnchor.constraint(equalToConstant: Static.heightPlant),
+            plantStopView.widthAnchor.constraint(equalToConstant: Static.widhtPlant),
+            plantStopView.bottomAnchor.constraint(equalTo: tomatoStopView.topAnchor),
+        ])
+    }
+}
+
+// MARK: - Configuration
+
+extension ViewController {
+    
+    func createCircularPath() {
+        circleLayer.path = circularPath.cgPath
+        progressLayer.path = circularPath.cgPath
+        
+        circleLayer.fillColor = UIColor.clear.cgColor
+        circleLayer.strokeColor = UIColor(hex: "#FA8072").cgColor
+        circleLayer.lineWidth = 12
+        circleLayer.strokeEnd = 1.0
+        
+        progressLayer.fillColor = UIColor.clear.cgColor
+        progressLayer.strokeColor = UIColor(hex: "#DC143C").cgColor
+        progressLayer.lineWidth = 12
+        progressLayer.strokeEnd = 0
+        
+        circleStopLayer.path = circularStopPath.cgPath
+        progressStopLayer.path = circularStopPath.cgPath
+        
+        circleStopLayer.fillColor = UIColor.clear.cgColor
+        circleStopLayer.strokeColor = UIColor(hex: "#8FBC8F").cgColor
+        circleStopLayer.lineWidth = 12
+        circleStopLayer.strokeEnd = 1.0
+        
+        progressStopLayer.fillColor = UIColor.clear.cgColor
+        progressStopLayer.strokeColor = UIColor(hex: "#006400").cgColor
+        progressStopLayer.lineWidth = 12
+        progressStopLayer.strokeEnd = 0
+    }
+}
+
+// MARK: - Actions
+
+extension ViewController {
     
     @objc
     func startWork()  {}
